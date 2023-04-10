66ee23d9e36cb964231d47d5ae8407d652c50055
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-04-11 00:03:30 +0300

    add user markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,27 @@
 import { RecipeList } from "./RecipeList";
-import recepies from "../recepies.json"
+import recepies from "../recepies.json";
+import { UserProfile } from "./UserProfile";
+import user from "../user.json";
 
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
+        // display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
     >
+      < UserProfile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats} />
+      
       < RecipeList items={recepies} />
     </div>
   );
