43e15273b306fe01e0f78938a7671e48cc416c34
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-04-13 00:19:12 +0300

    add statistics markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
-import { RecipeList } from "./RecipeList";
-import recepies from "../recepies.json";
-import { UserProfile } from "./UserProfile";
-import user from "../user.json";
-import { FriendList } from "./FriendList";
-import friendlist from "../friends.json"
+import { RecipeList } from './RecipeList';
+import recepies from '../recepies.json';
+import { UserProfile } from './UserProfile';
+import user from '../user.json';
+import { FriendList } from './FriendList';
+import friendlist from '../friends.json';
+import { Statistics } from './Statistics';
+import data from '../data.json';
 
 export const App = () => {
   return (
@@ -14,19 +16,24 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      < UserProfile
+      <UserProfile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
-        stats={user.stats} />
-      
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+
+      <Statistics stats={data} />
+
       <FriendList friends={friendlist} />
-      
-      < RecipeList items={recepies} />
+
+      <RecipeList items={recepies} />
     </div>
   );
 };
